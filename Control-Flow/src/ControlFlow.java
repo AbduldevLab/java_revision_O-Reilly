fe71c486c7f4e95001b2559d461568f292e87641
abdulhaouit <abderahman.haouit.2020@mumail.ie>
2024-11-03 18:46:13 +0000

    Updated

--- Control-Flow/src/ControlFlow.java
+++ Control-Flow/src/ControlFlow.java
@@ -2,7 +2,7 @@ public class ControlFlow {
     public static void main(String[] args) {
         //System.out.println("Hello world!");
 
-        int stop = 4;
+        int stop = 5;
         for (int i = 0; i < 10; i++) {
             if (i < stop) {
                 System.out.println("Hello abdulDevLab");
