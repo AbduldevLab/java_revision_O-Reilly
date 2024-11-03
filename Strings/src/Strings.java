7f876187f3dfc3813b721fc0270daf6b10fad0da
abdulhaouit <abderahman.haouit.2020@mumail.ie>
2024-11-03 22:29:07 +0000

    first update

--- Strings/src/Strings.java
+++ Strings/src/Strings.java
@@ -1,6 +1,7 @@
 public class Strings {
     public static void main(String[] args) {
         String myName = "abdulDevLab";
-        System.out.println(myName);
+        int length = myName.length();
+        System.out.println("My name is: "+myName+ " which is "+length+" characters long");
     }
 }
