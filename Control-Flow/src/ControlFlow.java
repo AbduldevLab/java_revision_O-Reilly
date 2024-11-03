611facc5bf03691121f53a0aad171a3d5ba86350
abdulhaouit <abderahman.haouit.2020@mumail.ie>
2024-11-03 18:43:46 +0000

    Updated

---
+++ Control-Flow/src/ControlFlow.java
@@ -0,0 +1,12 @@
+public class ControlFlow {
+    public static void main(String[] args) {
+        //System.out.println("Hello world!");
+
+        int stop = 4;
+        for (int i = 0; i < 10; i++) {
+            if (i < stop) {
+                System.out.println("Hello abdulDevLab");
+            }
+        }
+    }
+}
