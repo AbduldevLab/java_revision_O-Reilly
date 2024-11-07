5a702c4cedaf75ac604c5aea49598a16c5801726
abdulhaouit <abderahman.haouit.2020@mumail.ie>
2024-11-07 23:18:12 +0000

    updated getter & setters

--- Classes/src/Classes.java
+++ Classes/src/Classes.java
@@ -6,10 +6,42 @@ public class Classes {
     private String jobTitle;
     private String department;
 
-    public void setId(int value){
-        id = value;
+    public void setId(int id){
+        this.id = id;
     }
     public int getId(){
         return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
     }
 }
