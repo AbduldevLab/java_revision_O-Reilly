08b0924b58884f6440ba480219d303f145ed2a09
abdulhaouit <abderahman.haouit.2020@mumail.ie>
2024-11-10 21:23:12 +0000

    updated correct files

---
+++ Inheritance/src/com/abduldevlab/travelbooking/Main.java
@@ -0,0 +1,40 @@
+package com.abduldevlab.travelbooking;
+
+import com.abduldevlab.travelbooking.domain.BusTicket;
+import com.abduldevlab.travelbooking.domain.PlaneTicket;
+import com.abduldevlab.travelbooking.domain.TrainTicket;
+import com.abduldevlab.travelbooking.domain.TravelTicket;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class Main {
+    public static void main(String[] args) {
+        //System.out.println("Hello world!");
+        TrainTicket trainTicket = new TrainTicket();
+
+        trainTicket.cancel();
+        //trainTicket.
+
+        TrainTicket trainTicket2 = new TrainTicket(123L, "Dublin", "Limerick", new BigDecimal("29.00"),
+                LocalDateTime.of(2024,10, 10, 16, 03),
+                LocalDateTime.of(2024, 10, 10, 19, 03),
+        1,3,42);
+        trainTicket2.upgrade();
+
+        String[] providers = {"First bus company", "Second bus company"};
+        BusTicket busTicket = new BusTicket(124L, "Dublin", "Limerick", new BigDecimal("29.00"),
+                LocalDateTime.of(2023,10, 10, 16, 03),
+                LocalDateTime.of(2023, 10, 10, 19, 03),
+                providers);
+
+        trainTicket2.cancel();
+        busTicket.cancel();
+
+        //TravelTicket ticket = new TravelTicket();
+
+        PlaneTicket planeTicket = new PlaneTicket();
+        //planeTicket.setSeatNumber();
+        //
+    }
+}
---
+++ Inheritance/src/com/abduldevlab/travelbooking/domain/BusTicket.java
@@ -0,0 +1,40 @@
+package com.abduldevlab.travelbooking.domain;
+
+import java.math.BigDecimal;
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+public class BusTicket extends TravelTicket{
+
+    private String[] permittedProviders;
+
+    public BusTicket(){
+        super();
+    }
+
+    public BusTicket(Long bookingRef, String origin, String destination, BigDecimal price, LocalDateTime departureTime, LocalDateTime arrivalTime, String[] permittedProviders) {
+        super(bookingRef, origin, destination, price, departureTime, arrivalTime);
+        this.permittedProviders = permittedProviders;
+    }
+
+    public String[] getPermittedProviders() {
+        return permittedProviders;
+    }
+
+    public void setPermittedProviders(String[] permittedProviders) {
+        this.permittedProviders = permittedProviders;
+    }
+
+    //Annotation change cancel method from travelTicket class
+    @Override
+    public void cancel() {
+        Duration d = Duration.between(LocalDateTime.now(), getDepartureTime());
+        Long days = d.toDays();
+        if(days>30){
+            super.cancel();
+        }else{
+            System.out.println("Sorry you cannot cancel within 30 days");
+        }
+
+    }
+}
