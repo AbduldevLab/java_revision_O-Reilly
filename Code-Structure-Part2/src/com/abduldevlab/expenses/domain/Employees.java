45900af06e27100bd7f11367e4ba87c7ecfd17b2
abdulhaouit <abderahman.haouit.2020@mumail.ie>
2024-11-11 22:52:38 +0000

    Updated the Code-Structure-Part2 folder for enums code

--- Code-Structure-Part2/src/com/abduldevlab/expenses/domain/Employees.java
+++ Code-Structure-Part2/src/com/abduldevlab/expenses/domain/Employees.java
@@ -24,7 +24,7 @@ public class Employees {
     public void printEmployees(){
         for(Employee e : employees){
             if(e != null)
-                System.out.println(e.getMailingName());
+                System.out.println(e);
         }
     }
 
