bc6a15b7dafacdc691d2bb61b4fbf36fca81669e
abdulhaouit <abderahman.haouit.2020@mumail.ie>
2024-11-15 22:43:08 +0000

    updated folder

--- Exceptions/src/Main.java
+++ Exceptions/src/Main.java
@@ -1,9 +1,32 @@
+import javax.print.attribute.URISyntax;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.time.LocalDate;
 
 public class Main {
 
     public static void dividedByZero(int denominator) {
         System.out.println(17 / denominator);
+    }
+
+    public static void createURI() {
+        try {
+            URI uri = new URI("https://www.google.com");
+            System.out.println("The uri was created");
+        } catch (URISyntaxException e) {
+            System.out.println("The uri was not created");
+        }
+    }
+
+    public static void createURIv2() {
+
+        try {
+            URI uri = new URI("-https://www.google.com");
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+        //System.out.println("The uri was created");
+
     }
 
     public static void main(String[] args) {
@@ -22,8 +45,9 @@ public class Main {
 //        String hello = "Hello";
 //        Double value = Double.valueOf(hello);
 
-        User user1 = new User ("Jack", 5);
-        User user2 = new User("Abdul", -1);
+        User user1 = new User("Jack", 5);
+        User user2 = new User("Abdul", 31);
+        createURI();
 
     }
 }
