8a1e8c969dfe0de02fe581ba57141e2977348c0c
abdulhaouit <abderahman.haouit.2020@mumail.ie>
2024-11-16 14:49:28 +0000

    neew update

--- ArrayList/src/Main.java
+++ ArrayList/src/Main.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.HashSet;
 
 public class Main {
     public static void main(String[] args) {
@@ -7,9 +8,10 @@ public class Main {
         //Can store anything
         ArrayList arrayList = new ArrayList();
         int i = 2;
-        String s = "Hello";
         arrayList.add(i);
+        String s = "Hello", s2=s;
         arrayList.add(s);
+        arrayList.add(s2);
         System.out.println("This array list contains a " + arrayList + " and is of size " + arrayList.size());
         ArrayList<String> stringArray = new ArrayList<>();
         stringArray.add("Abduldevlab");
@@ -17,5 +19,12 @@ public class Main {
 
         //ArrayList preserves the order and allow for duplicates
         //hashsets don't and will remove duplicates - based on their hash value
+
+        HashSet<String> hashSet = new HashSet<>();
+        //String h = "Hello", h2=h;
+        hashSet.add(s);
+        hashSet.add(s2);
+        System.out.println(hashSet +" "+hashSet.size());
+
     }
 }
